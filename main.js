681856c36b259765f548479d7d0250226851c8ff
teslacreator23 <135182581+teslacreator23@users.noreply.github.com>
2024-08-12 18:42:50 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+ function Setup(){
+createCanvas(280,280)
+ }
+console.log(){
+    quik_draw_data_set
+    Math.floor().
+        random_no  = Math.floor((Math.random() *array_1.length) +1 )
+        Element_of_array = array_1[ random_no ]
+}
+
+
+timer_counter= 0
+
+timer_check =
+
+drawn_sketch =
+
+answer_holder =
+
+score = 1+
+
+
+ function draw(){
+check_sketch
+if verification of variable drawn_sketch = sketch
+ }
+ check_sketch(){
+ramdom_number
+ }
+ updateCanvas(){
+    Math.floor()
+   ramdom_number-quick_draw_data_set
+ }
+
+ updateCanvas(){
+ }
+
+ ramdom_number = quick_draw_data_set
+
+/*Actualiza la variable answer_holder - (contenedor de respuesta) con la cadena "set". Esto es para indicar que el dibujo realizado es igual al dibujo.
